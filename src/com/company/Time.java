380ea4d395546f7fe8081d7dd562e162ded9c719
Jurga <jurgak@gmail.com>
2021-09-10 23:49:30 +0300

    First Assignment Commit

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -1,7 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Time {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter your current hour in 24 hour format");
+        int time = input.nextInt();
+        if (time <= 12 && time >= 8) {
+            System.out.println("Good Morning Sunshine!");
+        } else {
+            if (time >= 13 && time <= 19) {
+                System.out.println("Good Afternoon. Work hard!");
+            } else {
+                if (time >= 20 && time <= 24) {
+                    System.out.println("Good Evening. Get some rest!");
+                }
+                else {
+                    if (time >= 0 && time <= 8){
+                        System.out.println("Why not in bed yet???");
+                    }
+                }
+            }
+        }
     }
 }
