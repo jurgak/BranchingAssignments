edc6f2d0610f2b0d0742dea78c15f92bd4e31f0f
Jurga <jurgak@gmail.com>
2021-09-11 01:10:31 +0300

    Third Assignment Commit

--- src/com/company/Date.java
+++ src/com/company/Date.java
@@ -7,15 +7,12 @@ public class Date {
         Scanner input = new Scanner(System.in);
         System.out.println("please write the day");
         int day = input.nextInt();
-        Scanner inputTwo = new Scanner(System.in);
         System.out.println("please write the month");
-        int month = inputTwo.nextInt();
-        Scanner inputThree = new Scanner(System.in);
+        int month = input.nextInt();
         System.out.println("please write the year");
-        int year = inputThree.nextInt();
-        Scanner inputFour = new Scanner(System.in);
+        int year = input.nextInt();
         System.out.println("please select the date formatting: 1 - YYYY/MM/DD, 2 - DD.MM.YYYY");
-        int format = inputFour.nextInt();
+        int format = input.nextInt();
         if (format == 1) {
             System.out.println("Your date is " + year + "/" + month + "/" + day);
         } else {
