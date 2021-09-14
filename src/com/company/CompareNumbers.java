3b21bafb20d48a6a2c2d6bc370f8a086759c7b7c
Jurga <jurgak@gmail.com>
2021-09-14 23:26:57 +0300

    Corrections in CompareNumbers made

--- src/com/company/CompareNumbers.java
+++ src/com/company/CompareNumbers.java
@@ -15,10 +15,10 @@ public class CompareNumbers {
         } else {
             System.out.println(numberOne > numberTwo ? "Number One is greater than Number Two" : "Number Two is greater than Number One");
         }
-        System.out.println(numberOne % 2 == 0 ? "Number One is even" : "Number One is odd");
-        System.out.println(numberTwo % 2 == 0 ? "Number Two is even" : "Number Two is odd");
-        System.out.println(numberOne < 0 ? "Number One is negative" : "Number One is positive");
-        System.out.println(numberTwo < 0 ? "Number Two is negative" : "Number Two is positive");
+        System.out.println("Number One is " + (numberOne % 2 == 0 ? "even" : "odd"));
+        System.out.println("Number Two is " + (numberTwo % 2 == 0 ? "even" : "odd"));
+        System.out.println("Number One is " + (numberOne < 0 ? "negative" : "positive"));
+        System.out.println("Number Two is " + (numberTwo < 0 ? "negative" : "positive"));
         if (numberOne < 100 && numberTwo < 100) {
             System.out.println("Two numbers are below 100");
         } else {
